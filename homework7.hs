96ade8301e90332cf81a497298361a21b8883c00
Jules Garrett <jrgarrett3222@gmail.com>
2017-10-09 17:59:21 -0500

    Delete homework7.hs

--- homework7.hs
+++
@@ -1,8 +0,0 @@
-
-
-merge:: Ord a => [a] -> [a] -> [a]
-merge [] []=[]
-merge a [] = a
-merge [] a = a
-merge (a:as) (b:bs) | a > b = b : merge (a:as) bs
-merge (a:as) (b:bs) | otherwise = a : merge as (b:bs)
